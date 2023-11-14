1bda9d99e7f2db2880b57a0fa67f30dd462eb095
dfg-98 <dariofg98@gmail.com>
2023-11-14 11:47:37 -0500

    refactor: chage Copyright year

--- src/coolc.sh
+++ src/coolc.sh
@@ -5,7 +5,7 @@ OUTPUT_FILE=${INPUT_FILE:0: -2}mips
 
 # Si su compilador no lo hace ya, aquí puede imprimir la información de contacto
 echo "IceBox: Turning COOL code into cooler results!"        #
-echo "Copyright (c) 2019: Dario Fragas, Abraham González"    # TODO: líneas a los valores correctos
+echo "Copyright (c) 2023: Dario Fragas, Abraham González"    # TODO: líneas a los valores correctos
 
 # Exec the compiler
 exec python3 cool.py "$@"  
