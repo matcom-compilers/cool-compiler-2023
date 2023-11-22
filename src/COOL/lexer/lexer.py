5482d5d3a0f4cca81754e84d2ab07abd974d616a
Dennis Fiallo <dfiallo35@gmail.com>
2023-11-22 12:32:06 -0500

    fix merge

--- src/COOL/lexer/lexer.py
+++ src/COOL/lexer/lexer.py
@@ -61,12 +61,13 @@ class CoolLexer(Lexer):
     
     # Symbols
     TYPE = r"[A-Z][a-zA-Z0-9_]*"
-    ID = r"[a-zA-Z_][a-zA-Z0-9_]*"
+    ID = r"[a-z][a-zA-Z0-9_]*"
 
     @_(r'\d+')
     def NUMBER(self, t):
         t.value = int(t.value)
         return t
+    
     @_(r'\".*?\"')
     def STRING(self, t):
         t.value = str(t.value).strip('\'')
