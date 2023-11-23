64f34692619ac85255698f70c29e6458ec0938f6
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-11-23 15:26:21 -0500

    Update classdef.py

--- src/COOL/token/classdef.py
+++ src/COOL/token/classdef.py
@@ -15,15 +15,20 @@ class Class(Token):
         type: str,
         inherits: str = None
     ) -> None:
-        self.features = features
         self.type = type
         self.inherits = inherits
+        self.features = features
+        self.methods= [i for i in features if isinstance(i, Method)]
+        self.attributes = [i for i in features if isinstance(i, Attribute)]
         super().__init__(line)
 
     def execute(self):
         raise NotImplementedError()
-    
-    def check(self, visitor:Visitor):
+
+    def check(self, visitor: Visitor):
         visitor.visit_class(self)
+
+        self.features = {i.id: i for i in self.features}
+
         for feature in self.features:
             feature.check(visitor)
