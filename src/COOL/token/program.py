966cae78cdeaa04821cdf9359aa23144bb43a19e
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-11-23 15:26:27 -0500

    Update program.py

--- src/COOL/token/program.py
+++ src/COOL/token/program.py
@@ -7,16 +7,25 @@ from src.COOL.token.classdef import Class
 
 class Program(Token):
     def __init__(self, classes: List[Class]) -> None:
-        self.classes = classes
         self.visitor = Visitor()
+        self.classes = classes
+
     def execute(self):
         for _class in self.classes:
             _class.execute()
-    
+
     def check(self):
         self.visitor.visit_program(self)
+
+        for class_ in self.classes.keys():
+            class_.inherits = self.classes[class_.inherits]
+
+        self.classes = {i.type: i for i in self.classes}
+
+        self.visitor.types = self.classes
+
         for _class in self.classes:
             _class.check(self.visitor)
-        
 
-    
+
+
