22367d6a9b69e254a2d4ce244dbfa4423bfc46f3
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-11-25 12:44:01 -0500

    Update test_semantic_check.py

--- test_semantic_check.py
+++ test_semantic_check.py
@@ -16,8 +16,8 @@ mc = Method(7, type='object', id='cd', formals=[], expr=None)
 
 classa = Class(1, [a, ma, mc], "A")
 classb = Class(10, [b, c], "B", "A")
-classc = Class(100, [], "C", "A")
-classd = Class(1000, [], "D", "C")
+classc = Class(100, [], "C", "B")
+classd = Class(1000, [], "D", "A")
 
 prog = Program(classes=[classa, classb, classc, classd])
 
