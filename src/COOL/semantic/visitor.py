9966430c4206a21f20757923d7a9545077f44c89
Lauren Guerra <91337957+lauolivia@users.noreply.github.com>
2023-11-25 23:30:33 -0500

    Update visitor.py

--- src/COOL/semantic/visitor.py
+++ src/COOL/semantic/visitor.py
@@ -26,40 +26,43 @@ class Visitor:
     def _search_lineage(self,class_:str):
         temp_class = class_
         lineage = []
-        lineage.add(class_)
+        lineage.append(class_)
         while temp_class in self.tree.keys():
             lineage.append(self.types[temp_class].inherits)
             temp_class = self.tree[temp_class]
         return lineage
 
-    def _search_feature_name_in_lineage(self,lineage:list,feature:str,type_:type):
+    def _search_feature_name_in_lineage(self, lineage:list, feature:str, type_:type):
         feature_equals=[]
         for i in lineage:
-            if feature in self.types[i].features.keys():
-                if type(self.types[i].features[feature]) == type_:
-                    feature_equals.append((self.types[i],self.types[i].features[feature]))
+            if not i:
+                break
+            for comprobate_feat in self.types.get(i).features:
+                if feature == comprobate_feat.id:
+                    if type(comprobate_feat) == type_:
+                        feature_equals.append(comprobate_feat)
         return feature_equals
     
     def visit_program(self, node):
         for i in node.classes:
             if i.type in self.types.keys():
                 #TODO search this error
-                self.errors.append(Error.error(node.line,node.column,"TypeError",'Repeated class name {node.type}'))
+                self.errors.append(Error.error(i.line,i.column,"TypeError",'Repeated class name {node.type}'))
             self.types[i.type] = i
 
         for cls in node.classes:
             if cls.type in self.basic_types.keys():
-                self.error.append(Error.error(node.line, node.column, 'SemanticError',
+                self.error.append(Error.error(cls.line, cls.column, 'SemanticError',
                     f'Redefinition of basic class {cls.type}.'))
             if cls.inherits:
                 if not cls.inherits in self.types.keys():
                     if cls.inherits in self.basic_types:
-                        self.error.append(Error.error(node.line, node.column, 'InheritanceError',
+                        self.error.append(Error.error(cls.line, cls.column, 'InheritanceError',
                             f'Class {cls.type} cannot inherit class {cls.inherits}. '))
-                    self.error.append(Error.error(node.line, node.column, 'TypeError',
+                    self.error.append(Error.error(cls.line, cls.column, 'TypeError',
                         f'Class {cls.type} inherits from an undefined class {cls.inherits}.'))
                 self.tree[cls.type] = cls.inherits
-                self._check_cycle(cls.type)
+                self._check_cycle(cls.type,cls)
 
     def visit_class(self, node):
         # TODO to define an error for repeated attributes and methods
@@ -68,10 +71,13 @@ class Visitor:
         features_node = set()
         for feat in node.features:
             if feat.id in features_node:
-                raise Exception(
-                    f'Repeated feature name {feat.id} in {node.type}')
+                #TODO search an error for this
+                self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Repeated feature name {feat.id} in {node.type}'))
+
             if feat.type not in self.types.keys() and not (feat.type in self.basic_types.keys()):
-                raise Exception(f'Undefined type {feat.type}')
+                #TODO search an error for this
+                self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Undefined type {feat.type}'))
+
             features_node.add(feat.id)
 
         lineage = self._search_lineage(node.type)
@@ -80,27 +86,27 @@ class Visitor:
             equals_attrbs = self._search_feature_name_in_lineage(lineage,attrb.id,type(attrb))
             if len(equals_attrbs) > 0:
                 equal_attrb = equals_attrbs[0]
-                if attrb.type != equal_attrb[1].type:
+                if attrb.type != equal_attrb.type:
                     #TODO search this error
                     self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible type of attribute in {attrb.id} in {node.type}'))
-                if attrb.expr:
-                    if not attrb.expr.type == attrb.type:
-                        #TODO search this error
-                        self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible type of attribute in {attrb.id} in {node.type}'))
+                # if attrb.expr:
+                #     if not attrb.expr.type == attrb.type:
+                #         #TODO search this error
+                #         self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible type of attribute in {attrb.id} in {node.type}'))
 
 
         for meth in node.methods:
-            equals_methods = self._search_feature_name_in_lineage(lineage,meth.id,type(meth))
+            equals_methods = self._search_feature_name_in_lineage(lineage, meth.id, type(meth))
             if len(equals_methods) > 0:
                 equal_meth = equals_methods[0]
-                if len(meth.formals) != len(equal_meth[1].formals):
+                if len(meth.formals) != len(equal_meth.formals):
                     #TODO search this error
                     self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible number of formals in {meth.id} in {node.type}'))
                 for j in range(len(meth.formals)):
-                    if meth.formals[j].type != equal_meth[1].formals[j].type:
+                    if meth.formals[j].type != equal_meth.formals[j].type:
                         #TODO search this error
                         self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible type of formals in {meth.id} in {node.type}'))
-                if meth.type != equal_meth[1].type:
+                if meth.type != equal_meth.type:
                     #TODO search this error
                     self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible return type in {meth.id} in {node.type}'))
             
@@ -109,12 +115,12 @@ class Visitor:
         node.attributes={i.id:i for i in node.attributes}
         node.features = {i.id: i for i in node.features}
 
-        if node.inherits:
-            for inh_attr in node.inherits.features.keys():
-                if inh_attr in node.features.keys():
-                    if not (node.inherits.features[inh_attr].type == node.features[inh_attr].type):
-                        raise Exception(
-                            f'Can not subscribe the attribute {inh_attr} with different type in {node.type} and {node.inherits.type}')
+        # if node.inherits:
+        #     for inh_attr in node.inherits.features.keys():
+        #         if inh_attr in node.features.keys():
+        #             if not (node.inherits.features[inh_attr].type == node.features[inh_attr].type):
+        #                 self.errors.append(Error.error(node.line,node.column,'',f'Can not subscribe the attribute {inh_attr} with different type in {node.type} and {node.inherits.type}'))
+        #                     f'')
 
     def visit_method(self, node):
         pass
@@ -124,10 +130,14 @@ class Visitor:
     #     pass
 
     def visit_attribute(self, node):
-        if node.expr:
-            if not node.expr.type == node.type:
-                raise Exception(
-                    f'The attribute {node.id} is not conform to the type {node.type}')
+        pass
 
+        # if node.expr:
+        #     if not node.expr.type == node.type:
+        #         #TODO search this error
+        #         self.errors.append(Error.error(node.line,node.column,'SemanticError',f'Incompatible type of attribute in {node.id} in {node.type}'))
+                    
+        # #TODO when an attribute does have the same static and dynamic type(or not inheritance)
+        #TypeError: Inferred type F of initialization of attribute test does not conform to declared type B. 
     # def visit_expression(self, node):
     #     pass
