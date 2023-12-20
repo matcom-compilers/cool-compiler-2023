8f86316e79dbf470f81e3e2c92ed2479f90b386b
Paula Rodríguez Pérez <62489004+prp97@users.noreply.github.com>
2023-12-20 10:05:13 -0500

    lexer tests ok -comment1 q no lo abre

--- src/cool_lexer.py
+++ src/cool_lexer.py
@@ -3,19 +3,20 @@ from errors import LexicographicError
 from ply import lex
 from utils import tokens, keywords, literals
 
+
 class CoolLexer:
     def __init__(self, data) -> None:
         self.errors = []
-        
+
         self.tokens = tokens
-        self.states = (('comment', 'exclusive'), ('string', 'exclusive'))
+        self.states = (("comment", "exclusive"), ("string", "exclusive"))
 
         self.lexer = lex.lex(module=self)
         self.lexer.col = 1
 
         self.data = data
 
-        self.str_val = ''
+        self.str_val = ""
         self.str_line = 0
         self.str_col = 0
         self.str_end = False
@@ -33,55 +34,57 @@ class CoolLexer:
             a.append(tok)
             # yield tok
             # print(tok.value, tok.line, tok.col)
+            # try:
+            #     print(tok.type)
+            # except:
+            #     pass
         return a
-    
+
     def pos(self, token):
-        line_start = self.data.rfind('\n', 0, token.lexpos) + 1
-        token.col = (token.lexpos - line_start) + 1 #token.lexer.col
+        line_start = self.data.rfind("\n", 0, token.lexpos) + 1
+        token.col = (token.lexpos - line_start) + 1  # token.lexer.col
         token.line = token.lexer.lineno
         token.lexer.col += len(token.value)
-        
 
     def find_column(self, token):
-        line_start = self.data.rfind('\n', 0, token.lexpos) + 1
+        line_start = self.data.rfind("\n", 0, token.lexpos) + 1
         return (token.lexpos - line_start) + 1
 
-
     def t_LESSEQ(self, t):
-        r'<='
+        r"<="
         self.pos(t)
         return t
 
     def t_ASSIGN(self, t):
-        r'<-'
+        r"<-"
         self.pos(t)
         return t
 
     def t_ARROW(self, t):
-        r'=>'
+        r"=>"
         self.pos(t)
         return t
 
     def t_TYPE(self, t):
-        r'[A-Z][a-zA-Z_0-9]*'
+        r"[A-Z][a-zA-Z_0-9]*"
         self.pos(t)
-        t.type = keywords.get( t.value.lower(), 'TYPE')
+        t.type = keywords.get(t.value.lower(), "TYPE")
         return t
 
     def t_ID(self, t):
-        r'[a-z][a-zA-Z_0-9]*'
+        r"[a-z][a-zA-Z_0-9]*"
         self.pos(t)
-        t.type = keywords.get(t.value.lower(), 'ID')
+        t.type = keywords.get(t.value.lower(), "ID")
         return t
 
     def t_INT(self, t):
-        r'\d+'
+        r"\d+"
         self.pos(t)
         t.value = int(t.value)
         return t
 
     def t_ignore_COMMENT_LINE(self, t):
-        r'(--.*(\n | $))'
+        r"(--.*(\n | $))"
         pass
 
     def t_string(self, t):
@@ -91,183 +94,201 @@ class CoolLexer:
         self.str_line = t.line
         self.str_col = t.col
         self.str_end = False
-        t.lexer.begin('string')
+        t.lexer.begin("string")
 
     def t_string_end(self, t):
         r'(?<!\\)"'
         self.str_end = True
-        t.lexer.begin('INITIAL')
-        t.type = 'STRING'
-        t.value = self.str_val + t.value      
+        t.line = self.str_line
+        t.col = self.str_col
+        t.lexer.begin("INITIAL")
+        t.type = "STRING"
+        t.value = self.str_val + t.value
         for index, char in enumerate(t.value):
-            if char == '\0':
+            if char == "\0":
                 null_col = t.col + index
                 null_line = t.line
                 self.errors.append(
-                    LexicographicError(line=null_line, column=null_col, message='NULL CHARACTER')
+                    LexicographicError(
+                        line=null_line, column=null_col, message="String contains null character"
+                    )
                 )
         # t.value += t.value[1:-1]
-        t.line = self.str_line
-        t.col = self.str_col
         return t
 
     def t_string_space(self, t):
-        r'\s'
-
-        if t.value== '\n' and not self.str_new_line:
+        r"\s"
+        if t.value == "\n" and not self.str_new_line:
             self.pos(t)
-            self.errors.append(LexicographicError(line=t.line, column=t.col, message="Unterminated string constant"))
-            t.lexer.begin('INITIAL')
-
-    def t_string_pass(self, t):
-        r'.'
-        self.str_val += t.value
-        if t.value == '\\':
-            self.str_new_line = True
-            
+            self.errors.append(
+                LexicographicError(
+                    line=t.line, column=t.col, message="Unterminated string constant"
+                )
+            )
             t.lexer.lineno += len(t.value)
             t.lexer.col = 1
-        else: 
+            t.lexer.begin("INITIAL")
+        if t.value == "\n" and self.str_new_line:
+            self.pos(t)
+            t.lexer.lineno += len(t.value)
+            t.lexer.col = 1
+
+    def t_string_pass(self, t):
+        r"."
+        self.str_val += t.value
+        if t.value == "\\":
+            self.str_new_line = True
+            # t.lexer.lineno += len(t.value)
+            # t.lexer.col = 1
+        else:
             self.str_new_line = False
 
+    def t_string_eof(self, t):
+        self.pos(t)
+        self.errors.append(
+            LexicographicError(
+                line=t.line, column=t.col, message="EOF in string constant"
+            )
+        )
+
     def t_comment(self, t):
-        r'\(\*'
+        r"\(\*"
         t.lexer.comm_start = t.lexer.lexpos - 2
         t.lexer.level = 1
-        t.lexer.begin('comment')
+        t.lexer.begin("comment")
 
     def t_comment_lcomment(self, t):
-        r'\(\*'
+        r"\(\*"
         t.lexer.level += 1
 
     def t_comment_rcomment(self, t):
-        r'\*\)'
+        r"\*\)"
         t.lexer.level -= 1
         if t.lexer.level == 0:
             t.value = t.lexer.lexdata[t.lexer.comm_start : t.lexer.lexpos]
-            t.type = 'COMMENT_LINE'
-            t.lexer.lineno += t.value.count('\n')
-            t.lexer.begin('INITIAL')
+            t.type = "COMMENT_LINE"
+            # t.lexer.lineno += t.value.count("\n")
+            t.lexer.begin("INITIAL")
 
     def t_comment_pass(self, t):
-        r'.|\n'
+        r".|\n"
         self._end_comment = False
-        if t.value == '\n':
+        if t.value == "\n":
             t.lexer.lineno += 1
             t.lexer.col = 1
         else:
             t.lexer.col += len(t.value)
 
-    def t_comments_newline(self, t):
+    def t_comment_newline(self, t):
         r"\n"
         t.lexer.last_new_line_pos = t.lexer.lexpos
         t.lexer.lineno += 1
 
     def t_newline(self, t):
-        r'\n+'
+        r"\n+"
         t.lexer.lineno += len(t.value)
         t.lexer.col = 1
 
     def t_WHITESPACE(self, t):
-        r'\s'
-        if t.value == '\t':
+        r"\s"
+        if t.value == "\t":
             t.lexer.col += 4
         else:
             t.lexer.col += len(t.value)
 
     def t_PLUS(self, t):
-        r'\+'
-        t.type = 'PLUS'
+        r"\+"
+        t.type = "PLUS"
         self.pos(t)
         return t
 
     def t_MINUS(self, t):
-        r'-'
-        t.type = 'MINUS'
+        r"-"
+        t.type = "MINUS"
         self.pos(t)
         return t
 
     def t_STAR(self, t):
-        r'\*'
-        t.type = 'STAR'
+        r"\*"
+        t.type = "STAR"
         self.pos(t)
         return t
 
     def t_DIV(self, t):
-        r'/'
-        t.type = 'DIV'
+        r"/"
+        t.type = "DIV"
         self.pos(t)
         return t
 
     def t_COMPL(self, t):
-        r'~'
-        t.type = 'COMPL'
+        r"~"
+        t.type = "COMPL"
         self.pos(t)
         return t
 
     def t_EQUAL(self, t):
-        r'='
-        t.type = 'EQUAL'
+        r"="
+        t.type = "EQUAL"
         self.pos(t)
         return t
 
     def t_LESS(self, t):
-        r'<'
-        t.type = 'LESS'
+        r"<"
+        t.type = "LESS"
         self.pos(t)
         return t
 
     def t_COLON(self, t):
-        r':'
-        t.type = 'COLON'
+        r":"
+        t.type = "COLON"
         self.pos(t)
         return t
 
     def t_LBRACE(self, t):
-        r'\{'
-        t.type = 'LBRACE'
+        r"\{"
+        t.type = "LBRACE"
         self.pos(t)
         return t
 
     def t_RBRACE(self, t):
-        r'\}'
-        t.type = 'RBRACE'
+        r"\}"
+        t.type = "RBRACE"
         self.pos(t)
         return t
 
     def t_AT(self, t):
-        r'@'
-        t.type = 'AT'
+        r"@"
+        t.type = "AT"
         self.pos(t)
         return t
 
     def t_COMMA(self, t):
-        r','
-        t.type = 'COMMA'
+        r","
+        t.type = "COMMA"
         self.pos(t)
         return t
 
     def t_DOT(self, t):
-        r'\.'
-        t.type = 'DOT'
+        r"\."
+        t.type = "DOT"
         self.pos(t)
         return t
 
     def t_LPAREN(self, t):
-        r'\('
-        t.type = 'LPAREN'
+        r"\("
+        t.type = "LPAREN"
         self.pos(t)
         return t
 
     def t_RPAREN(self, t):
-        r'\)'
-        t.type = 'RPAREN'
+        r"\)"
+        t.type = "RPAREN"
         self.pos(t)
         return t
 
     def t_SEMICOLON(self, t):
-        r';'
-        t.type = 'SEMICOLON'
+        r";"
+        t.type = "SEMICOLON"
         self.pos(t)
         return t
 
@@ -276,5 +297,7 @@ class CoolLexer:
         line = t.lineno
         col = t.col
         # print('any', line, col)
-        self.errors.append(LexicographicError(line=line, column=col, message=t.value[0]))
+        self.errors.append(
+            LexicographicError(line=line, column=col, message=t.value[0])
+        )
         t.lexer.skip(1)
--- src/main.py
+++ src/main.py
@@ -35,9 +35,15 @@ def main(_input, _output):
 
 
 if __name__ == "__main__":
+    # Lexer tests
+    # in_path = 'tests/lexer/iis1.cl'
+    # in_path = 'tests/lexer/mixed1.cl'
+    # in_path = 'tests/lexer/string4.cl'
+    in_path = 'tests/lexer/comment1.cl'
+
     # in_path = 'tests/codegen/arith.cl'
     # in_path = 'tests/codegen/book_list.cl'
-    in_path = 'tests/lexer/string1.cl'
+    
     # in_path = 'tests/parser/assignment1.cl'
     out_path = 'src/codeMips.mips'
     _input = sys.argv[1] if len(sys.argv) > 1 else in_path
