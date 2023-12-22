6705fb34d9ebf8ed6cd32f26a45cc377ce3bb03e
Paula Rodríguez Pérez <62489004+prp97@users.noreply.github.com>
2023-12-21 21:43:38 -0500

    Update errors.py

--- src/utils/errors.py
+++ src/utils/errors.py
@@ -13,24 +13,24 @@ class CoolError(Exception):
     def __repr__(self):
         return str(self)
 
-# class CompilerError(CoolError):
-#     def __init__(self, message):
-#         CoolError.__init__(self, 0, 0, "CompilerError", message)
+class CompilerError(CoolError):
+    def __init__(self, message):
+        CoolError.__init__(self, 0, 0, "CompilerError", message)
 
 
 class LexicographicError:
     def __init__(self, line, column, message):
-        # CoolError.__init__(self, line, column, "LexicographicError", message)
-        self.line = line
-        self.col = column
-        self.message = message
+        CoolError.__init__(self, line, column, "LexicographicError", message)
+        # self.line = line
+        # self.col = column
+        # self.message = message
 
     
-    def __str__(self) -> str:
-        return f'{self.line, self.col} - LexicographicError: {self.message}'
+    # def __str__(self) -> str:
+    #     return f'{self.line, self.col} - LexicographicError: {self.message}'
 
-    def __repr__(self):
-        return str(self)
+    # def __repr__(self):
+    #     return str(self)
 
 class SyntacticError(CoolError):
     def __init__(self, line, column, message):
@@ -45,16 +45,16 @@ class SemanticError(CoolError):
         CoolError.__init__(self, line, column, "SemanticError", message)
 
 
-# class TypeError(CoolError):
-#     def __init__(self, line, column, message):
-#         CoolError.__init__(self, line, column, "TypeError", message)
+class TypeError(CoolError):
+    def __init__(self, line, column, message):
+        CoolError.__init__(self, line, column, "TypeError", message)
 
 
-# class NameError(CoolError):
-#     def __init__(self, line, column, message):
-#         CoolError.__init__(self, line, column, "NameError", message)
+class NameError(CoolError):
+    def __init__(self, line, column, message):
+        CoolError.__init__(self, line, column, "NameError", message)
 
 
-# class AttributeError(CoolError):
-#     def __init__(self, line, column, message):
-#         CoolError.__init__(self, line, column, "AttributeError", message)
+class AttributeError(CoolError):
+    def __init__(self, line, column, message):
+        CoolError.__init__(self, line, column, "AttributeError", message)
